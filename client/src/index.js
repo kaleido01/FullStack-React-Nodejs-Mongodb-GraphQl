def930c0239beb9504a2bb25c0ecb195ef391e73
kaleido <kaleido@gmail.com>
2019-05-21 08:58:02 +0900

    react-router-domの導入とsignup signinルートの設定

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
+} from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
+import Signin from "./components/Auth/Signin";
+import Signup from "./components/Auth/Signup";
+
 const client = new ApolloClient({
 	uri: "http://localhost:4000/graphql"
 });
 
+const Root = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={App} />
+			<Route path="/signin" component={Signin} />
+			<Route path="/signup" component={Signup} />
+			<Redirect to="/" />
+		</Switch>
+	</Router>
+);
+
 ReactDOM.render(
 	<ApolloProvider client={client}>
-		<App />
+		<Root />
 	</ApolloProvider>,
 
 	document.getElementById("root")
