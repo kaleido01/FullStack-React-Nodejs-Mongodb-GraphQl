18096479e1091617665e88b48569d8ecaaf376b9
kaleido <kaleido@gmail.com>
2019-05-20 22:29:57 +0900

    8 dotenvの作成とmongoDBへのconnect

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "./variables.env" });
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("DB connected");
+	})
+	.catch(err => {
+		console.error(err);
+	});
 
 const app = express();
 
