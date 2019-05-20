971129a80f6998ae21588524e048ec396636cfad
kaleido <kaleido@gmail.com>
2019-05-20 23:27:01 +0900

    apolloserverの導入とmongooseによるmodelをGraphQLへコネクト

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 require("dotenv").config({ path: "./variables.env" });
 
 const Recipe = require("./models/Recipe");
 const User = require("./models/User");
+
+//Bring in GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+});
 
 mongoose
 	.connect(process.env.MONGO_URI)
@@ -16,6 +29,21 @@ mongoose
 	});
 
 const app = express();
+
+//create GrapQL application
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+//Connect schema with GraphQL
+app.use(
+	"/graphql",
+	graphqlExpress({
+		schema,
+		context: {
+			Recipe,
+			User
+		}
+	})
+);
 
 const PORT = process.env.PORT || 4444;
 
