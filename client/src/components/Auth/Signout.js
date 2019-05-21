0bb6b18c15ca3fa7bb3bf4a4c772f75fbba93198
kaleido <kaleido@gmail.com>
2019-05-21 14:19:40 +0900

    signout componentの作成sessionの削除

---
+++ client/src/components/Auth/Signout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { ApolloConsumer } from "react-apollo";
+
+const handleSignout = (client, history) => {
+	localStorage.setItem("token", "");
+	client.resetStore();
+	history.push("/");
+};
+
+const Signout = ({ history }) => {
+	return (
+		<ApolloConsumer>
+			{client => {
+				console.log(client);
+				return (
+					<button onClick={() => handleSignout(client, history)}>
+						signout
+					</button>
+				);
+			}}
+		</ApolloConsumer>
+	);
+};
+
+export default withRouter(Signout);
