3750f4175376755b9251a77192ac4d7d43ab9b59
kaleido <kaleido@gmail.com>
2019-05-21 09:07:08 +0900

    基本的なフォームの作成

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 
 export class Signup extends Component {
 	render() {
-		return <div>Signup</div>;
+		return (
+			<div className="App">
+				<h2 className="App">Signup</h2>
+				<form className="form">
+					<input type="text" placeholder="Username" name="text" />
+					<input type="email" placeholder="Your Email" name="email" />
+					<input type="password" placeholder="Password" name="password" />
+					<input
+						type="password"
+						placeholder="Confirm Password"
+						name="passwordConfirmation"
+					/>
+					<button type="submit" className="button-primary">
+						Submit
+					</button>
+				</form>
+			</div>
+		);
 	}
 }
 
