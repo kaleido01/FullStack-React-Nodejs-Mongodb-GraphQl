fa7f7caf10dc3d3ae8e1f5552e0c04197fb43331
kaleido <kaleido@gmail.com>
2019-05-21 09:19:57 +0900

    signup のstate管理

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
 
 export class Signup extends Component {
+	state = {
+		username: "",
+		email: "",
+		password: "",
+		passwordConfirmation: ""
+	};
+
+	changeHandler = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
 	render() {
+		const { username, email, password, passwordConfirmation } = this.state;
 		return (
 			<div className="App">
 				<h2 className="App">Signup</h2>
 				<form className="form">
-					<input type="text" placeholder="Username" name="text" />
-					<input type="email" placeholder="Your Email" name="email" />
-					<input type="password" placeholder="Password" name="password" />
+					<input
+						type="text"
+						placeholder="Username"
+						name="username"
+						onChange={this.changeHandler}
+						value={username}
+					/>
+					<input
+						type="email"
+						placeholder="Your Email"
+						name="email"
+						onChange={this.changeHandler}
+						value={email}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						onChange={this.changeHandler}
+						value={password}
+					/>
 					<input
 						type="password"
 						placeholder="Confirm Password"
 						name="passwordConfirmation"
+						onChange={this.changeHandler}
+						value={passwordConfirmation}
 					/>
 					<button type="submit" className="button-primary">
 						Submit
